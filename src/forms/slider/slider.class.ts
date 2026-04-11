b7e3b2088d9d3a22270cb222bc56cd3a5ec1344f
subframe7536 <1667077010@qq.com>
2026-04-11 11:55:20 +0800

    fix(slider): update background color for slider thumb variant

--- src/forms/slider/slider.class.ts
+++ src/forms/slider/slider.class.ts
@@ -62,7 +62,7 @@ export const sliderRangeVariants = cva('rounded-full bg-primary select-none abso
 })
 
 export const sliderThumbVariants = cva(
-  'outline-none surface-border rounded-full bg-foreground shrink-0 block cursor-pointer select-none shadow-xs/5 transition-[box-shadow,transform] relative focus-visible:effect-fv hover:effect-fv data-dragging:scale-120 not-dark:bg-clip-padding',
+  'outline-none surface-border rounded-full bg-background shrink-0 block cursor-pointer select-none shadow-xs/5 transition-[box-shadow,transform] relative focus-visible:effect-fv hover:effect-fv dark:bg-foreground data-dragging:scale-120 not-dark:bg-clip-padding',
   {
     defaultVariants: {
       size: 'md',
